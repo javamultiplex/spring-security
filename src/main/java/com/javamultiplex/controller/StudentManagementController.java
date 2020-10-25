17ec138412b22908278b8bf4d029d24c8a4f1682
Rohit Agarwal <programmingwithrohit@gmail.com>
2020-10-25 21:58:03 +0530

    Added authorities on method level using annotations

--- src/main/java/com/javamultiplex/controller/StudentManagementController.java
+++ src/main/java/com/javamultiplex/controller/StudentManagementController.java
@@ -5,6 +5,7 @@ import com.javamultiplex.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,18 +26,21 @@ public class StudentManagementController {
     }
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_ADMIN_TRAINEE')")
     public List<Student> getStudents() {
         System.out.println("Inside getStudents() method");
         return studentService.getStudents();
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public Student registerStudent(@RequestBody Student student) {
         System.out.println("Inside registerStudent() method");
         return studentService.registerStudent(student);
     }
 
     @DeleteMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public ResponseEntity<String> deleteStudent(@PathVariable Integer studentId) {
         System.out.println("Inside deleteStudent() method");
         try {
@@ -48,6 +52,7 @@ public class StudentManagementController {
     }
 
     @PutMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public ResponseEntity<String> updateStudent(@PathVariable Integer studentId, @RequestBody Student updatedStudent) {
         System.out.println("Inside updateStudent() method");
         try {
--- src/main/java/com/javamultiplex/security/ApplicationSecurityConfig.java
+++ src/main/java/com/javamultiplex/security/ApplicationSecurityConfig.java
@@ -3,7 +3,7 @@ package com.javamultiplex.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,7 +13,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-import static com.javamultiplex.security.ApplicationUserPermission.STUDENT_WRITE;
 import static com.javamultiplex.security.ApplicationUserRole.*;
 
 /**
@@ -23,6 +22,7 @@ import static com.javamultiplex.security.ApplicationUserRole.*;
 
 @EnableWebSecurity
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
@@ -41,10 +41,10 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
-                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(STUDENT_WRITE.getPermission())
-                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(STUDENT_WRITE.getPermission())
-                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(STUDENT_WRITE.getPermission())
-                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMIN_TRAINEE.name())
+//                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(STUDENT_WRITE.getPermission())
+//                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(STUDENT_WRITE.getPermission())
+//                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(STUDENT_WRITE.getPermission())
+//                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMIN_TRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
